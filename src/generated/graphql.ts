c815cab38378ff483003f90e5b41295f25bf0e11
SOUM SOMON <somon.soum9@gmail.com>
2024-09-20 21:35:32 +0700

    update codegen

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -3,28 +3,30 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-  JSON: any;
-  Upload: any;
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  JSON: { input: any; output: any; }
+  Upload: { input: any; output: any; }
 };
 
 export type Mutation = {
   __typename?: 'Mutation';
-  testingMuation?: Maybe<Scalars['Boolean']>;
+  testingMuation?: Maybe<Scalars['Boolean']['output']>;
 };
 
 export type Ok = {
   __typename?: 'Ok';
-  ok?: Maybe<Scalars['Boolean']>;
+  ok?: Maybe<Scalars['Boolean']['output']>;
 };
 
 export type Query = {
   __typename?: 'Query';
-  hellWorld?: Maybe<Scalars['String']>;
+  hellWorld?: Maybe<Scalars['String']['output']>;
 };
